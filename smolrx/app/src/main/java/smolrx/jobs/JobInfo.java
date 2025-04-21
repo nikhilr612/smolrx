023abec14ce3ba31654913261c0094e750e180be
Nikhil R. <nikhil612.r@gmail.com>
2025-04-21 16:22:22 +0530

    Fixed build

--- smolrx/app/src/main/java/smolrx/jobs/JobInfo.java
+++ smolrx/app/src/main/java/smolrx/jobs/JobInfo.java
@@ -139,4 +139,8 @@ public class JobInfo implements Serializable, Comparable<JobInfo> {
     public Set<Long> getPrerequisiteJobs() {
         return Collections.unmodifiableSet(prerequisite_jobs);
     }
+
+    public long getProgramId() {
+        return programId;
+    }
 }
